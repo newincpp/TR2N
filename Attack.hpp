fcfa09e3185d30a9ac5451a7c0920bbc4359560d
koziar_c <koziar_c@epitech.eu>
2014-05-03 15:45:31 +0200

    les animations fonctionnent \o/ (sur place)

--- Attack.hpp
+++ Attack.hpp
@@ -12,21 +12,20 @@ class Attack {
 	sf::IntRect _offset;
 	sf::Clock _clock;
 	sf::IntRect _rect;
-int _inpact;
     public:
 	explicit Attack(AnimatedSprite &, int, sf::IntRect);
 	Attack(Attack const &);
 	Attack const &operator=(Attack const &);
 	~Attack();
 	bool testHitbox(sf::IntRect const &);
-	unsigned int getInpact()const;
-	void setInpact(unsigned int);
 	int getCollisionFrame() const;
 	sf::IntRect const &getRect() const;
 	void stop();
-	void play(sf::IntRect const &);
+	void play(sf::IntRect const &, Animation &);
 	bool update(sf::IntRect const &);
 	AnimatedSprite &getAnimatedSprite() const;
+	int getCurrentFrame() const;
+	bool isPlaying() const;
 };
 
 #endif /* !ATTACK_H_ */
