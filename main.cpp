5a87ccfd4fbe49d597ad9491ecdcb185c1a8226a
koziar_c <koziar_c@epitech.eu>
2014-05-04 14:47:39 +0200

    stay details to do

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ int main() {
     window.draw(loading);
     window.display();
 
+
     sf::Texture bgTexture;
     if (!bgTexture.loadFromFile("bg.png"))
     {
@@ -40,8 +41,8 @@ int main() {
     }
     Animation AtkAnim;
     AtkAnim.setSpriteSheet(texta);
-    for (int i = 0; i < 10017; i += 626) {
-	AtkAnim.addFrame(sf::IntRect(i, 0, 626, 724));
+    for (int i = 0; i < 6464; i += 404) {
+	AtkAnim.addFrame(sf::IntRect(i, 0, 404, 404));
     }
     sf::Texture textb;
     if (!textb.loadFromFile("blends/AtkAnimSpritesBlue.png")) {
@@ -50,8 +51,8 @@ int main() {
     }
     Animation AtkAnimBlue;
     AtkAnimBlue.setSpriteSheet(textb);
-    for (int i = 0; i < 10017; i += 626) {
-	AtkAnimBlue.addFrame(sf::IntRect(i, 0, 626, 724));
+    for (int i = 0; i < 6464; i += 404) {
+	AtkAnimBlue.addFrame(sf::IntRect(i, 0, 404, 404));
     }
     sf::Texture textc;
     if (!textc.loadFromFile("blends/dashAnimSprites.png")) {
@@ -60,8 +61,8 @@ int main() {
     }
     Animation DashAnim;
     DashAnim.setSpriteSheet(textc);
-    for (int i = 0; i < 10017; i += 626) {
-	DashAnim.addFrame(sf::IntRect(i, 0, 626, 724));
+    for (int i = 0; i < 6464; i += 404) {
+	DashAnim.addFrame(sf::IntRect(i, 0, 404, 404));
     }
     sf::Texture textd;
     if (!textd.loadFromFile("blends/dashAnimSpritesBlue.png")) {
@@ -70,8 +71,8 @@ int main() {
     }
     Animation DashAnimBlue;
     DashAnimBlue.setSpriteSheet(textd);
-    for (int i = 0; i < 10017; i += 626) {
-	DashAnimBlue.addFrame(sf::IntRect(i, 0, 626, 724));
+    for (int i = 0; i < 6464; i += 404) {
+	DashAnimBlue.addFrame(sf::IntRect(i, 0, 404, 404));
     }
     sf::Texture texte;
     if (!texte.loadFromFile("blends/standAnim.png")) {
@@ -80,8 +81,8 @@ int main() {
     }
     Animation standAnim;
     standAnim.setSpriteSheet(texte);
-    for (int i = 0; i < 10017; i += 626) {
-	standAnim.addFrame(sf::IntRect(i, 0, 626, 724));
+    for (int i = 0; i < 6464; i += 404) {
+	standAnim.addFrame(sf::IntRect(i, 0, 404, 404));
     }
     sf::Texture textf;
     if (!textf.loadFromFile("blends/standAnimBlue.png")) {
@@ -90,8 +91,8 @@ int main() {
     }
     Animation standAnimBlue;
     standAnimBlue.setSpriteSheet(textf);
-    for (int i = 0; i < 10017; i += 626) {
-	standAnimBlue.addFrame(sf::IntRect(i, 0, 626, 724));
+    for (int i = 0; i < 6464; i += 404) {
+	standAnimBlue.addFrame(sf::IntRect(i, 0, 404, 404));
     }
     sf::Texture textg;
     if (!textg.loadFromFile("redStun.png")) {
@@ -100,7 +101,7 @@ int main() {
     }
     Animation stun;
     stun.setSpriteSheet(textg);
-    stun.addFrame(sf::IntRect(0, 0, 626, 724));
+    stun.addFrame(sf::IntRect(0, 0, 400, 400));
     sf::Texture texth;
     if (!texth.loadFromFile("blueStun.png")) {
 	std::cout << "Failed to load player spritesheet!" << std::endl;
@@ -108,37 +109,83 @@ int main() {
     }
     Animation stunBlue;
     stunBlue.setSpriteSheet(texth);
-    stunBlue.addFrame(sf::IntRect(0, 0, 626, 724));
+    stunBlue.addFrame(sf::IntRect(0, 0, 400, 400));
+    sf::Texture texti;
+    if (!texti.loadFromFile("blends/spritesFastAtkAnimBlue.png")) {
+	std::cout << "Failed to load player spritesheet!" << std::endl;
+	return 1;
+    }
+    Animation fastAtkBlue;
+    fastAtkBlue.setSpriteSheet(texti);
+    for (int i = 0; i < 2424; i += 404) {
+	fastAtkBlue.addFrame(sf::IntRect(i, 0, 404, 404));
+    }
+    sf::Texture textj;
+    if (!textj.loadFromFile("blends/spritesFastAtkAnim.png")) {
+	std::cout << "Failed to load player spritesheet!" << std::endl;
+	return 1;
+    }
+    Animation fastAtk;
+    fastAtk.setSpriteSheet(textj);
+    for (int i = 0; i < 2424; i += 404) {
+	fastAtk.addFrame(sf::IntRect(i, 0, 404, 404));
+    }
+    sf::Texture textk;
+    if (!textk.loadFromFile("blends/Atk2Sprites.png")) {
+	std::cout << "Failed to load player spritesheet!" << std::endl;
+	return 1;
+    }
+    Animation Atk2Anim;
+    Atk2Anim.setSpriteSheet(textk);
+    for (int i = 0; i < 6464; i += 404) {
+	Atk2Anim.addFrame(sf::IntRect(i, 0, 404, 404));
+    }
+    sf::Texture textl;
+    if (!textl.loadFromFile("blends/Atk2SpritesBlue.png")) {
+	std::cout << "Failed to load player spritesheet!" << std::endl;
+	return 1;
+    }
+    Animation Atk2AnimBlue;
+    Atk2AnimBlue.setSpriteSheet(textl);
+    for (int i = 0; i < 6464; i += 404) {
+	Atk2AnimBlue.addFrame(sf::IntRect(i, 0, 404, 404));
+    }
 
     AnimatedSprite animatedSprite(sf::seconds(0.01), true, false);
     AnimatedSprite animatedSprite1(sf::seconds(0.01), true, false);
-    animatedSprite.setPosition(sf::Vector2f(412, 612));
+    animatedSprite.setPosition(sf::Vector2f(450, 500));
     animatedSprite1.setScale(-1, 1);
-    animatedSprite1.setPosition(sf::Vector2f(1812, 612));
+    animatedSprite1.setPosition(sf::Vector2f(1450, 500));
 
     sf::Clock frameClock;
 
     Attack c(animatedSprite, 2, sf::IntRect(0, 0, 0, 0));
     Attack d(animatedSprite1, 2, sf::IntRect(0, 0, 0, 0));
-    Player player1(c, 100, 200, 0);
-    Player player2(d, 1400, 200, 2, &player1);
-    Input i1("248", 60);
-    Input i2("268", 60);
-    Input i3("464", 60);
-    Input i4("646", 60);
+    Player player1(c, 550, 500, 0);
+    Player player2(d, 1550, 500, 2, &player1);
+    Input i("2525252525", 60);
+    Input i1("254585", 60);
+    Input i2("258", 60);
+    Input i3("454545", 60);
+    Input i4("656565", 60);
+    Input i5("4525652", 60);
     player1.setVs(&player2);
     Attack a(animatedSprite, 2, player1.getPosition());
     Attack b(animatedSprite1, 2, player2.getPosition());
-    player1.addAttack(a, 0, standAnim, i2, std::make_pair(0, 0));
-    player1.addAttack(a, 0, stun, i2, std::make_pair(0, 0));
+    player1.addAttack(a, 0, standAnim, i, std::make_pair(0, 0));
+    player1.addAttack(a, 0, stun, i, std::make_pair(0, 0));
     player1.addAttack(a, 10, AtkAnim, i1, std::make_pair(0, 1));
     player1.addAttack(a, 0, DashAnim, i3, std::make_pair(0, -4));
     player1.addAttack(a, 0, DashAnim, i4, std::make_pair(0, 4));
-    player2.addAttack(b, 0, standAnimBlue, i2, std::make_pair(0, 0));
-    player2.addAttack(b, 0, stunBlue, i2, std::make_pair(0, 0));
-    player2.addAttack(b, 10, AtkAnimBlue, i1, std::make_pair(0, 1));
-    player2.addAttack(b, 0, DashAnimBlue, i3, std::make_pair(0, -4));
-    player2.addAttack(b, 0, DashAnimBlue, i4, std::make_pair(0, 4));
+    player1.addAttack(a, 5, fastAtk, i2, std::make_pair(0, 4));
+    player1.addAttack(a, 50, Atk2Anim, i5, std::make_pair(0, 4));
+    player2.addAttack(b, 0, standAnimBlue, i, std::make_pair(0, 0));
+    player2.addAttack(b, 0, stunBlue, i, std::make_pair(0, 0));
+    player2.addAttack(b, 10, AtkAnimBlue, i1, std::make_pair(0, -1));
+    player2.addAttack(b, 0, DashAnimBlue, i3, std::make_pair(0, 4));
+    player2.addAttack(b, 0, DashAnimBlue, i4, std::make_pair(0, -4));
+    player2.addAttack(b, 5, fastAtkBlue, i2, std::make_pair(0, -4));
+    player2.addAttack(b, 50, Atk2AnimBlue, i5, std::make_pair(0, -4));
 
     player1.start(0);
     player2.start(0);
@@ -172,8 +219,8 @@ int main() {
 
     while (window.isOpen())
     {
-      Lb1.Set(player1.getLife());
-      Lb2.Set(player2.getLife());
+	Lb1.Set(player1.getLife());
+	Lb2.Set(player2.getLife());
 
 	if (music.getStatus() == sf::SoundSource::Status::Stopped)
 	{
